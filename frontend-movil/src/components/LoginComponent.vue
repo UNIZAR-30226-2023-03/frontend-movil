ab2f38cf3cf09f6c06628df001cbd34695cd7312
jeizaguerri <816079@unizar.es>
2023-03-15 13:40:29 +0100

    Conexion con backend para el login

--- frontend-movil/src/components/LoginComponent.vue
+++ frontend-movil/src/components/LoginComponent.vue
@@ -1,56 +1,72 @@
 <template>
-    <ion-page>
-      <ion-header>
-        <ion-toolbar>
-          <ion-title>Login</ion-title>
-        </ion-toolbar>
-      </ion-header>
+  <ion-page>
+    <ion-header>
+      <ion-toolbar>
+        <ion-title>Login</ion-title>
+      </ion-toolbar>
+    </ion-header>
+
+    <ion-content class="ion-padding">
+      <form @submit.prevent="login">
+        <ion-item>
+          <ion-label position="floating">Usuario/Contraseña</ion-label>
+          <ion-input v-model="userLogin"></ion-input>
+        </ion-item>
+
+        <ion-item>
+          <ion-label position="floating">Contraseña</ion-label>
+          <ion-input type="password" v-model="password"></ion-input>
+        </ion-item>
+
+        <ion-button type="submit" expand="block">Iniciar sesión</ion-button>
+      </form>
+    </ion-content>
+  </ion-page>
+</template>
   
-      <ion-content class="ion-padding">
-        <form @submit.prevent="login">
-          <ion-item>
-            <ion-label position="floating">Email</ion-label>
-            <ion-input type="email" v-model="email"></ion-input>
-          </ion-item>
-  
-          <ion-item>
-            <ion-label position="floating">Password</ion-label>
-            <ion-input type="password" v-model="password"></ion-input>
-          </ion-item>
-  
-          <ion-button type="submit" expand="block">Login</ion-button>
-        </form>
-      </ion-content>
-    </ion-page>
-  </template>
-  
-  <script>
-  import { IonPage, IonHeader, IonToolbar, IonTitle, IonContent, IonItem, IonLabel, IonInput, IonButton } from '@ionic/vue';
-  import { defineComponent } from 'vue';
-  
-  export default defineComponent({
-    components: {
-      IonPage,
-      IonHeader,
-      IonToolbar,
-      IonTitle,
-      IonContent,
-      IonItem,
-      IonLabel,
-      IonInput,
-      IonButton
-    },
-    data() {
-      return {
-        email: '',
-        password: ''
-      };
-    },
-    methods: {
-      login() {
-        // Perform login logic here
-      }
+<script>
+import { IonPage, IonHeader, IonToolbar, IonTitle, IonContent, IonItem, IonLabel, IonInput, IonButton } from '@ionic/vue';
+import { defineComponent } from 'vue';
+import axios from 'axios';
+import router from "@/router";
+
+export default defineComponent({
+  components: {
+    IonPage,
+    IonHeader,
+    IonToolbar,
+    IonTitle,
+    IonContent,
+    IonItem,
+    IonLabel,
+    IonInput,
+    IonButton
+  },
+  data() {
+    return {
+      userLogin: '',
+      password: ''
+    };
+  },
+  methods: {
+    login() {
+      axios.post('https://backend-sy93.onrender.com/usuarios/login', {
+        login: this.userLogin,
+        password: this.password
+      })
+        .then(function (response) {
+          console.log(response);
+          const success = response["data"];
+          console.log(success);
+          if(success){
+            router.push('/tabs/tab1')
+          }
+        })
+        .catch(function (error) {
+          console.log(error);
+        });
     }
-  });
-  </script>
+  }
+});
+</script>
   
