f73b799960a2cddb4a483f697903f3afa22e7f3b
jeizaguerri <816079@unizar.es>
2023-04-06 01:51:15 +0200

    Dado funcionando

---
+++ frontend-movil/src/components/DadoComponent.vue
@@ -0,0 +1,58 @@
+<template>
+    <canvas id="dado" style="width: 100%"></canvas>
+</template>
+
+<script>
+
+const rutas = ['assets/dado/1.png',
+ 'assets/dado/2.png',
+ 'assets/dado/3.png',
+ 'assets/dado/4.png',
+ 'assets/dado/5.png',
+ 'assets/dado/6.png']
+
+
+export default{
+    data() {
+        return {
+            
+        }
+    },
+    methods:{
+        sleep(milliseconds) {
+            return new Promise((resolve) => setTimeout(resolve, milliseconds));
+        },
+        crearDado(d){
+            console.log("crear");
+            const canvas = document.getElementById('dado');
+            const ctx = canvas.getContext('2d');
+
+            canvas.height = canvas.width;
+
+            const dado = new Image();
+            dado.src = rutas[d];
+            // Dibuja la imagen en el canvas cuando se haya cargado
+            dado.onload = function () {
+                ctx.drawImage(dado, 0, 0, canvas.width, canvas.height);
+            }
+        },
+        async tirarDado(){
+            console.log("tirar");
+            const canvas = document.getElementById('dado');
+            const ctx = canvas.getContext('2d');
+
+            let num = 0;
+            for (let index = 0; index < 20; index++) {
+                await this.sleep(100);
+                num = Math.floor(Math.random() * 6);
+                this.crearDado(num);
+            }
+            return num;
+        }
+    },
+    mounted(){
+        this.crearDado(0);
+        console.log(this.tirarDado());
+    }
+}
+</script>
---
+++ frontend-movil/src/components/TableroComponent.vue
@@ -0,0 +1,28 @@
+<template>
+    <canvas id="parchisBoard" style="width: 100%; border-radius: 10px;"></canvas>
+</template>
+
+<script>
+export default{
+    methods:{
+        crearTablero(){
+            const canvas = document.getElementById('parchisBoard');
+            const ctx = canvas.getContext('2d');
+
+            canvas.height = canvas.width;
+            console.log(canvas.width);
+            // // Carga la imagen del tablero
+            const boardImage = new Image();
+            boardImage.src = 'assets/tablero.png';
+
+            // Dibuja la imagen en el canvas cuando se haya cargado
+            boardImage.onload = function () {
+            ctx.drawImage(boardImage, 0, 0, canvas.width, canvas.height);
+            }
+        }
+    },
+    mounted(){
+        this.crearTablero();
+    }
+}
+</script>
