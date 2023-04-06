1774fd83920d35b757677a49d24b2f921375cd99
jeizaguerri <816079@unizar.es>
2023-04-06 03:32:46 +0200

    noche de lloros

--- frontend-movil/src/components/TableroComponent.vue
+++ frontend-movil/src/components/TableroComponent.vue
@@ -1,4 +1,5 @@
 <template>
+    <button v-for="(ficha, index) in fichas" :style="posicionFicha(ficha)" :key="index">f</button>
     <canvas id="parchisBoard" style="width: 100%; border-radius: 10px;"></canvas>
 </template>
 
@@ -10,7 +11,6 @@ export default{
             const ctx = canvas.getContext('2d');
 
             canvas.height = canvas.width;
-            console.log(canvas.width);
             // // Carga la imagen del tablero
             const boardImage = new Image();
             boardImage.src = 'assets/tablero.png';
@@ -19,10 +19,24 @@ export default{
             boardImage.onload = function () {
             ctx.drawImage(boardImage, 0, 0, canvas.width, canvas.height);
             }
+        },
+        posicionFicha(ficha){
+            console.log("pos");
+            const style = {};
+            style.position = "relative";
+            style.left = ficha[0]+"%";
+            style.top = ficha[1]+"%";
+            style.color = "red"; //pa probar
+            return style;
         }
     },
     mounted(){
         this.crearTablero();
+    },
+    data(){
+        return{
+            fichas:[["22.75", "44.9"]]
+        }
     }
 }
 </script>
