9c07b24114f8d6c2c5ad32e4a0cc1bf5257e13d3
JaimeDC02 <jaimedomper.val@gmail.com>
2023-04-10 01:40:46 +0100

    register y login done con Msg error

--- frontend-movil/src/components/LoginComponent.vue
+++ frontend-movil/src/components/LoginComponent.vue
@@ -7,6 +7,8 @@
         <h1>Iniciar sesion</h1>
         <input class="login-input" type="text" placeholder="Username" aria-label="Username" name="username" v-model="userLogin" required autofocus>
         <input class="login-input" type="password" placeholder="Password" aria-label="Password" name="password" v-model="password" required>
+        <div v-if="loading" class="loading-spinner"></div>
+        <div v-if="showErrorPass" class="error-message" style="color: red;">Usuario o contraseña incorrectos</div>
         <button class="login-button" type="submit">Iniciar Sesión</button>
         ¿Aun no tienes una cuenta? 
         Registrate <span class="register-link" @click="moveToRegister">aquí</span>
@@ -26,11 +28,15 @@
     data() {
       return {
         userLogin: '',
-        password: ''
+        password: '',
+        showErrorPass: false,
+        loading: false
       };
     },
     methods: {
       login() {
+        this.showErrorPass = false;
+        this.loading = true;
         axios.post('https://lamesa-backend.azurewebsites.net/usuario/login', {
           login: this.userLogin,
           password: this.password
@@ -43,8 +49,14 @@
           }
         })
         .catch((error) => {
-          console.log(error);
-        });
+        console.log(error);
+        if (error.response && error.response.status === 400) {
+          this.showErrorPass = true;
+          
+        }
+      }).finally(() => {
+      this.loading = false;
+    });
       },
       moveToRegister() {
         router.push('/registrarse'); // navigate to /registrarse route
--- frontend-movil/src/views/RegistroPage.vue
+++ frontend-movil/src/views/RegistroPage.vue
@@ -2,12 +2,14 @@
   <div class="login-background">
   <div class="bg-image"></div> 
     <div class="login-container">
-      <form @submit.prevent="login" name="login_form" class="login-form" method="post">
+      <form @submit.prevent="onSubmit" name="login_form" class="login-form" method="post">
         <h1>Registrarse</h1>
         <input type="text" class="login-input" placeholder="Nombre" aria-label="Username" name="username" v-model="userReg" required autofocus>
         <input type="text" class="login-input" placeholder="Email" aria-label="Email" name="email" v-model="email" required autofocus>
+        <div v-if="showErrorCuenta" class="error-message" style="color: red;">Error: {{ errorMessage }}</div>
         <input type="password" class="login-input" placeholder="Contraseña" aria-label="Password" name="password" v-model="password1" required>
         <input type="password" class="login-input" placeholder="Repita contraseña" aria-label="Password" name="password2" v-model="password2" required>
+        <div v-if="showErrorPass" class="error-message" style="color: red;">Las contraseñas no coinciden</div>
         <button class="login-button" type="submit">Crear cuenta</button>
         ¿ya tienes cuenta? 
         Inicia sesión  <span class="register-link" @click="moveToLogin">aquí</span>
@@ -25,48 +27,54 @@ import axios from 'axios';
 import router from "@/router";
 
 export default defineComponent({
-
   data() {
     return {
       userReg: '',
       email: '',
       password1: '',
-      password2: ''
+      password2: '',
+      showErrorPass: false,
+      showErrorCuenta: false,
+      errorMessage: ''
+
     };
   },
   methods: {
-    login() {
-      axios.post('https://lamesa-backend.azurewebsites.net/usuario/crear', {
-        
-      "email": this.email,
-      "username": this.userReg,
-      "password": this.password1
-      
-      })
+    onSubmit(event) {
+      if (this.password1 !== this.password2) {
+        this.showErrorPass = true;
+        event.preventDefault();
+      } else {
+        this.showErrorPass = false;
+        axios.post('https://lamesa-backend.azurewebsites.net/usuario/crear', {
+          "email": this.email,
+          "username": this.userReg,
+          "password": this.password1
+        })
         .then(function (response) {
           console.log(response);
           const success = response.status === 200;
           console.log(success);
-          if(success){
+          if (success) {
             router.push('/')
-            //window.location.href = "/";
-          }
+          } 
+          this.showErrorCuenta = false;
         })
-        .catch(function (error) {
-          console.log(error);
-        });
-    }
-    ,
+        .catch((error) => {
+        console.log(error);
+        if (error.response && error.response.status === 400) {
+          this.showErrorCuenta = true;
+          this.errorMessage = error.response.data;
+        }
+      });
+      }
+    },
     moveToLogin() {
       router.push('/')
-}
+    }
   }
 });
 </script>
-
 <style>
 @import '../theme/estilosInicio.css';
 </style>
-
-  
-  
